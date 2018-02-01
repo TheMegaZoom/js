e1974cbd74deffd4c49ff67048c20e7821f39929
TheMegaZoom <30740796+TheMegaZoom@users.noreply.github.com>
2018-02-01 21:37:01 -0200

    Add files via upload

---
+++ richcards.js
@@ -0,0 +1,6 @@
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "http://www.themegazoom.tk",
+  "logo": "https://raw.githubusercontent.com/TheMegaZoom/img/master/logo.png"
+}
